ec2ca298180e8b228fca81ee205efdb451c11e4f
TØN1 <tonisalles@gmail.com>
2022-03-10 19:02:52 -0300

    Add files via upload

---
+++ freecodecamp/responsive-webdesign-certification/applied-visual-design/use-the-css-transform-property-skewx-to-skew-an-element-along-the-x-axis.css
@@ -0,0 +1,18 @@
+<style>
+  div {
+    width: 70%;
+    height: 100px;
+    margin:  50px auto;
+  }
+  #top {
+    background-color: red;
+  }
+  #bottom {
+    transform: skewX(24deg);
+    background-color: blue;
+
+  }
+</style>
+
+<div id="top"></div>
+<div id="bottom"></div>
